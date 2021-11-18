7cb35e624c1f2fde0956e4a3e09a54691e0ecc2b
Karim <kariimAhmed8@gmail.com>
2021-11-18 11:10:23 +0200

    Adding Product Details

---
+++ Check-24/Product Details/ProductsDetailsCoordinator.swift
@@ -0,0 +1,15 @@
+//
+//  ProductsDetailsCoordinator.swift
+//  Check-24
+//
+//  Created by KarimAhmed on 18/11/2021.
+//
+
+import Foundation
+
+class ProdutsDetailsCoordinator {
+    
+    init() {
+        
+    }
+}
---
+++ Check-24/Product Details/ProductsDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProductsDetailsViewController.swift
+//  Check-24
+//
+//  Created by KarimAhmed on 18/11/2021.
+//
+
+import UIKit
+
+class ProductsDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
